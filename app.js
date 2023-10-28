cb1f8d26c1cfd718aa919a88b680e6810851758b
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-28 02:57:56 +0100

    Test solution 3

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const flash = require("connect-flash");
 const multer = require("multer");
 const helmet = require("helmet");
 const compression = require("compression");
-const morgan = require("morgan");
-
 const errorController = require("./controllers/error");
 const shopController = require("./controllers/shop");
 const isAuth = require("./middleware/is-auth");
@@ -62,14 +60,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
-// const accessLogStream = fs.createWriteStream(
-//   path.join(__dirname, "access.log"),
-//   { flags: "a" }
-// );
-
 app.use(helmet());
 app.use(compression());
-// app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
